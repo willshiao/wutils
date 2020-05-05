aec7e331e6cba4d29efa1b3ea7f296fdd5348933
William Shiao <willshiao@gmail.com>
2020-05-05 04:07:07 -0700

    Increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='wutils',
-    version='0.1.0',
+    version='0.1.1',
     author='William Shioa',
     author_email='willshiao@gmail.com',
     packages=['wutils', 'wutils.test'],
