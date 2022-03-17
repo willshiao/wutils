611799bc076a26077ae90ed315856d73f605b744
William Shiao <willshiao@gmail.com>
2022-03-17 14:58:10 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='wutils',
-    version='0.2.0',
+    version='0.2.1',
     author='William Shiao',
     author_email='willshiao@gmail.com',
     packages=['wutils'],
